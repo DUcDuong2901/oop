f13be0c6f6519f1f51269846c2e655b9bf3a48aa
Duong Minh Duc <22022606@vnu.edu.vn>
2023-03-13 16:18:50 +0700

    first

--- HW1/BaitapD/fixRandomWalk.java
+++ HW1/BaitapD/fixRandomWalk.java
@@ -1,48 +1,49 @@
+package BaitapD;
+import edu.princeton.cs.algs4.StdDraw;
+import edu.princeton.cs.algs4.StdOut;
+
 public class fixRandomWalk {
-
     public static void main(String[] args) {
-        int n = Integer.parseInt(args[0]);
-        StdDraw.setScale(-n - 0.5, n + 0.5);
-        StdDraw.clear(StdDraw.GRAY);
-        StdDraw.enableDoubleBuffering();
+            int n = Integer.parseInt(args[0]);
+            StdDraw.setScale(-n - 0.5, n + 0.5);
+            StdDraw.clear(StdDraw.GRAY);
+            StdDraw.enableDoubleBuffering();
 
-        int x = 0, y = 0;
-        int steps = 0;
-        while (Math.abs(x) < n && Math.abs(y) < n) {
-            StdDraw.setPenColor(StdDraw.WHITE);
-            StdDraw.filledSquare(x, y, 0.45);
-            double r = Math.random();
-            if (Math.abs(x) == Math.abs(y)) {
-                if (x > 0 && y > 0) {
-                    x++;
-                } else if (x < 0 && y < 0) {
-                    x--;
-                } else if (x > 0 && y < 0) {
-                    y++;
-                } else {
-                    y--;
+            int x = 0, y = 0;
+            int steps = 0;
+            int direction = 0; // 0 = right, 1 = up, 2 = left, 3 = down
+            int segment_length = 1;
+
+            while (Math.abs(x) <= n && Math.abs(y) <= n) {
+                for (int i = 0; i < segment_length && Math.abs(x) <= n && Math.abs(y) <= n; i++) {
+                    StdDraw.setPenColor(StdDraw.WHITE);
+                    StdDraw.filledSquare(x, y, 0.45);
+                    switch (direction) {
+                        case 0:
+                            x++;
+                            break;
+                        case 1:
+                            y++;
+                            break;
+                        case 2:
+                            x--;
+                            break;
+                        case 3:
+                            y--;
+                            break;
+                    }
+                    steps++;
+                    StdDraw.setPenColor(StdDraw.BLUE);
+                    StdDraw.filledSquare(x, y, 0.45);
+                    StdDraw.show();
+                    StdDraw.pause(40);
                 }
-            } else if (Math.abs(x) > Math.abs(y)) {
-                if (x > 0) {
-                    y--;
-                } else {
-                    y++;
-                }
-            } else {
-                if (y > 0) {
-                    x++;
-                } else {
-                    x--;
+                direction = (direction + 1) % 4;
+                if (direction == 0 || direction == 2) {
+                    segment_length++;
                 }
             }
-
-            steps++;
-            StdDraw.setPenColor(StdDraw.BLUE);
-            StdDraw.filledSquare(x, y, 0.45);
-            StdDraw.show();
-            StdDraw.pause(40);
+            StdOut.println("Total steps = " + steps);
         }
-        StdOut.println("Total steps = " + steps);
-    }
 
-}
+    }
